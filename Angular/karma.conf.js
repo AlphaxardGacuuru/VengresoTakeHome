f16fe5506404618eb97869028784cd6cb2fb9642
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2023-10-12 21:46:02 +0300

    Added Angular

--- Angular/karma.conf.js
+++ Angular/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-vengreso'),
+      dir: require('path').join(__dirname, './coverage/Angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
